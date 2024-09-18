7deea9f2034e7e3244e9923d299a1e04f55d217e
Antonio Gabarrús Nerin <gabatono@gmail.com>
2024-09-18 15:07:38 +0200

    Test Completed

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ const app = require('./app')
 describe('GET /users', () => {
   it('should return all users', async () => {
     const res = await request(app).get('/users')
-    expect(res.statusCode).toEqual(404)
+    expect(res.statusCode).toEqual(200)
     expect(res.body).toHaveLength(2)
   })
 })
